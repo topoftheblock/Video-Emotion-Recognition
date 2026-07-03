9996dcb9d640bae09b960bb85fb7a81ff496006e
topoftheblock <christian.block@freenet.de>
2026-07-03 16:48:32 +0200

    changed parser

--- CAS_to_DB/parser/main.py
+++
@@ -1,9 +0,0 @@
-from parser import XMIParser
-from importer import Importer
-from database import Session
-
-parser = XMIParser("bundestag_full.xmi").parse()
-
-db = Session()
-
-Importer(parser, db).run()
